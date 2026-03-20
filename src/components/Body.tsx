81cffb706333916a319ecc034a2adf237cdfc12a
Sanuji Gamagedara <wimansasanuji@gmail.com>
2026-03-20 11:54:30 +0200

    left panel

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,1 +1,14 @@
-//intial comment
+import React, {useState} from "react";
+
+interface BodyProps {
+    name: string;
+}
+
+export function Body({name}:BodyProps){
+    return (
+        <div className="bg-black rounded-2xl p-4">
+            
+            <p className="text-slate-500 font-medium mb-6">{name}</p>
+        </div>
+    )
+}
