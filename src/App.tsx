ac026509315483ba5bc7f0889ac5d29856ab3b2b
Sanuji <wimansasanuji@gmail.com>
2026-03-20 19:32:39 +0200

    small changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Body } from './components/Body';
+import { LeftPanel } from './components/LeftPanel';
 
 export default function App() {
   return (
-    <div className="p-8 bg-slate-50 min-h-screen flex flex-col items-center">
-      <h1 className="text-2xl font-bold mb-8 text-slate-700">Valitse rasia</h1>
-      
-      <Body name="Neliö 250ml" />
-      
-    </div>
+      <div className ="main-layout">
+        <LeftPanel />
+      </div>
   );
 }
--- src/components/LeftPanel.tsx
+++ src/components/LeftPanel.tsx
@@ -1,14 +1,18 @@
 import React, {useState} from "react";
 
-interface BodyProps {
-    name: string;
-}
+const sizes = [
+    {label: "Neliö 250 ml", value: 250},
+    {label: "Neliö 500 ml", value:500},
+    {label: "Ympyrä 250 ml", value: 250},
+    {label: "Ympyrä 500 ml", value: 500}
+];
 
-export function Body({name}:BodyProps){
-    return (
-        <div className="bg-black rounded-2xl p-4">
-            
-            <p className="text-slate-500 font-medium mb-6">{name}</p>
+ export function LeftPanel(){
+    const [selected, setSelected] = useState(3);
+
+    return(
+        <div className="left-panel">
+            <h3>1. Valitse rasia</h3>
         </div>
     )
 }
