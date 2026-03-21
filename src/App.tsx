69cbb3de889fb88fadbfc04dbea4445afbf3d1cc
Sanuji <wimansasanuji@gmail.com>
2026-03-21 18:53:20 +0200

    ingredient section

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import { LeftPanel } from './components/LeftPanel';
 import { BaseSelection } from './components/BaseSelection';
 import { CenterBowl } from './components/CenterBowl';
+import { IngredientSection } from './components/IngredientSection';
 
 export default function App() {
   return (
-      <div className ="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen bg-gray-100 p-8 flex flex-col gap-8">
+        <div className ="flex gap-8 items-start justify-center">
         <LeftPanel />
         <CenterBowl />
         <BaseSelection />
       </div>
+
+      <IngredientSection />
+    </div>
+      
   );
 }
