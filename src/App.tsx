f2552662c797704d04adb01ac846bf07a8c8e003
Sanuji <wimansasanuji@gmail.com>
2026-03-21 15:29:11 +0200

    Select base (right panel)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { LeftPanel } from './components/LeftPanel';
+import { BaseSelection } from './components/BaseSelection';
 
 export default function App() {
   return (
       <div className ="min-h-screen flex items-center justify-center bg-gray-100">
         <LeftPanel />
+        <BaseSelection />
       </div>
   );
 }
