638b01a11fb211a3eb57e8625d8b7f67a1007e50
Sanuji <wimansasanuji@gmail.com>
2026-03-23 13:46:36 +0200

    Type definitions (User and Recipe)

---
+++ src/types/index.tsx
@@ -0,0 +1,15 @@
+export interface User {
+    id:number;
+    email: string;
+    name: string;
+    role:string;
+}
+
+export interface Recipe{
+    id:number;
+    userId: number;
+    name: string;
+    bowlId: number;
+    ingredientIds: number[];
+    is_public: boolean;
+}
