4902848075fcd5e267f187cd55d9bbb9778f6a69
Sanuji <wimansasanuji@gmail.com>
2026-03-24 14:33:47 +0200

    Add Header, SummaryBar, and Footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import Header from './components/Header';
 import { LeftPanel } from './components/LeftPanel';
 import { BaseSelection } from './components/BaseSelection';
 import { CenterBowl } from './components/CenterBowl';
 import { IngredientSection } from './components/IngredientSection';
+import {SummaryBar} from './components/SummaryBar';
+import {Footer} from './components/Footer';
 
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-100 p-8 flex flex-col gap-8">
         <div className ="flex gap-8 items-start justify-center">
+        <Header />
         <LeftPanel />
         <CenterBowl />
         <BaseSelection />
+        <SummaryBar />
+        <Footer />
       </div>
 
       <IngredientSection />
