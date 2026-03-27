01f338d22a14f787f27952cef6ff309749a027a8
Sanuji Gamagedara <wimansasanuji@gmail.com>
2026-03-27 13:01:04 +0200

    added 3 icons

--- src/components/CenterBowl.tsx
+++ src/components/CenterBowl.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 export function CenterBowl(){
     return (
-        <div className="flex-1 flex flex-col items-center justify-center min-h-[400px] mt-4 lg:mt-0">
-        <div className="flex gap-3 mb-6 items-center">
-            <button className="bg-[#A2D135] text-black rounded-lg">Salaatti</button>
-            <button className="bg-[#A2D135] text-black rounded-lg">Rahka</button>
+        <div className="flex-1 flex flex-col items-center justify-center min-h-[400px] mt-4 lg:mt-0 ">
+        <div className="flex gap-3 mb-6 items-center ">
+            <button className="bg-[#A2D135] gap-3 mb-6 items-center text-black rounded-lg ">Salaatti</button>
+            <button className="bg-[#A2D135] gap-3 mb-6 items-center text-black rounded-lg ">Rahka</button>
             <div className="flex gap-2">
-                <button className="w-8 h-8 bg-gray-300 rounded-full" />
-                <button className="w-8 h-8 bg-gray-300 rounded-full" />
+                <button className="w-8 h-8 bg-gray-300 rounded-full gap-3 mb-6 items-center" />
+                <button className="w-8 h-8 bg-gray-300 rounded-full gap-3 mb-6 items-center" />
+                <button className="w-8 h-8 bg-gray-300 rounded-full gap-3 mb-6 items-center" />
             </div>
         </div>
         <div className="w-80 h-80 rounded-full border-[12px] border-gray-200 bg-gray-50 flex items-center justify-center shadow-inner relative" />
