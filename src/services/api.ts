b8a8a96abf6b67864e5a61b8aa360ac42d674431
Sanuji <wimansasanuji@gmail.com>
2026-03-29 17:39:35 +0300

    API Service Layer (Architecture)

---
+++ src/services/api.ts
@@ -0,0 +1,31 @@
+const URL = "https://fresse-api.onrender.com/api/...";
+
+export async function getBowls(){
+    const response = await fetch(`${URL}/bowls`);
+
+    if (!response.ok) {
+        throw new Error ("error");
+    }
+
+    return response.json();
+}
+
+export async function getCategories(){
+    const response = await fetch(`${URL}/categories`);
+
+    if(!response.ok){
+        throw new Error ("error");
+    }
+
+    return response.json();
+}
+
+export async function getIngredients(){
+    const response = await fetch(`${URL}/ingrediants`);
+
+    if(!response.ok){
+        throw new Error("error");
+    }
+
+    return response.json();
+}
