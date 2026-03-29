d7dd5d7963cd46a10735253c761b7279c29a56d0
Sanuji <wimansasanuji@gmail.com>
2026-03-29 17:19:46 +0300

    defined types foe ingrediants and categories

---
+++ src/types/index.ts
@@ -0,0 +1,48 @@
+
+export interface PriceListItem {
+    id:number;
+    item_id:number;
+    price:number;
+    type:string;
+    }
+
+export interface BaseType {
+  id: number;
+  name: string;
+  price?: number;
+  image_url?: string;
+  barcode_url?: string;
+}
+
+export interface Bowl extends BaseType {
+  base_type_id?: number;
+  volume?: number;
+  slot_count: number;
+  shape: 'round' | 'square';
+}
+    
+export interface User {
+    id:number;
+    email: string;
+    name: string;
+    role:string;
+}
+
+export interface Recipe{
+    id:number;
+    userId: number;
+    name: string;
+    bowlId: number;
+    ingredientIds: number[];
+    is_public: boolean;
+
+}
+
+export interface Category {
+  id: number;
+  name: string;
+}
+
+export interface Ingredient extends BaseType {
+  categoryId: number;
+}
