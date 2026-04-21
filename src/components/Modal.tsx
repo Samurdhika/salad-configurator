36c0b83ebdc2b54d49284003b4f18e15d76f4130
Samurdhika Susanthi <samurdhikasusanthi@outlook.com>
2026-04-21 14:01:12 +0300

    login page UI

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,27 +1,23 @@
 import React from "react";
-import type {ReactNode} from "react";
 
-type ModalProps = {
-    isOpen: boolean;
-    onClose: () => void;
-    children: ReactNode;
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
 }
 
-export const Modal: React.FC<ModalProps> = ({isOpen, onClose, children}) => {
-    if (!isOpen) return null;
+export function Modal({ isOpen, onClose, children }: ModalProps) {
+  if (!isOpen) return null;
 
-    return(
-        <div className="fixed inset-0 bg-black/60 flex items-center justify-center z-50"
-        onClick = {onClose}
-        >
-            <div className="bg-white rounded-x1 shadow-lg p-6 relative w-full"
-            onClick={(e) => e.stopPropagation()}
-            >
-                <button className="bg-white text-red" onClick={onClose}>
-                X
-                </button>
-                {children}
-            </div>
-        </div>
-    )
+  return (
+    <div
+      className="fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-50"
+      onClick={onClose}
+    >
+      
+      <div onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
+    </div>
+  );
 }
